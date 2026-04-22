b87f7ca54e9e334a29940740d0b1ed7e6cbd5535
Muhammad Awais <usamamushtaq.dev@gmail.com>
2026-04-22 22:13:31 +0500

    feat: add global validation pipe and class-validator

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // strips any fields not in DTO
+      forbidNonWhitelisted: true, //  rejects requests with extra fields
+      transform: true, // transform plain object to DTO Class instance
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
